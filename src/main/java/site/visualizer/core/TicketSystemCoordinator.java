5ed86ca3c394687d49238e5bb5199da9af7f8bb0
KenulaNimhan <kenulanimhan@gmail.com>
2025-09-24 11:34:56 +0530

    implement ticket coordinator

---
+++ src/main/java/site/visualizer/core/TicketSystemCoordinator.java
@@ -0,0 +1,23 @@
+package site.visualizer.core;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TicketSystemCoordinator {
+    private final AtomicInteger totalProduced, totalConsumed;
+    private final int totalToProduce, totalToConsume;
+
+    public TicketSystemCoordinator(int totalToProduce, int totalToConsume) {
+        totalProduced = new AtomicInteger();
+        totalConsumed = new AtomicInteger();
+        this.totalToProduce = totalToProduce;
+        this.totalToConsume = totalToConsume;
+    }
+
+    public void incrementProducedCount() {totalProduced.getAndIncrement();}
+
+    public void incrementConsumedCount() {totalConsumed.getAndIncrement();}
+
+    public boolean isProductionDone() {return totalProduced.get() == totalToProduce;}
+
+    public boolean isConsumptionDone() {return totalConsumed.get() == totalToConsume;}
+}
