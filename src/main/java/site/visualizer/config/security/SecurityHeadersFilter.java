4c978a70630efb44e245d468813dbd3b1097706e
KenulaNimhan <kenulanimhan@gmail.com>
2025-10-07 18:54:16 +0530

    add security headers

---
+++ src/main/java/site/visualizer/config/security/SecurityHeadersFilter.java
@@ -0,0 +1,25 @@
+package site.visualizer.config.security;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class SecurityHeadersFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        res.setHeader("Content-Security-Policy", "default-src 'self';");
+        res.setHeader("X-Frame-Options", "DENY");
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
+        res.setHeader("Permissions-Policy", "camera=(), microphone=(), geolocation=()");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+}
